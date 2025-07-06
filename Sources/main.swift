dd616d97e3cba927b14938a2ab5365bdd12b28f0
Donaldson Tan <geodome@gmail.com>
2025-07-07 03:21:53 +0800

    unwrap optional config and optional db in single line

--- Sources/main.swift
+++ Sources/main.swift
@@ -2,10 +2,9 @@
 // https://docs.swift.org/swift-book
 
 func main() {
-    if let config = getAppConfig() {
+    if let config = getAppConfig(), let db = config.getDatabaseConnection() {
         // setup database
-        let db = config.getDatabaseConnection()
-        let tg = TgGroups(conn:db!)
+        let tg = TgGroups(conn:db)
         tg["ict133"] = ("Structured Programming", "https://t.me/+91udwbtlw1hmYjE9")
         tg["ict162"] = ("Object Oriented Programming", "https://t.me/+91udwbtlw1hmYjE9")
         print(config)
