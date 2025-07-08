dae0bd2966dfdfa78a3d97db6c29de575706533c
Donaldson Tan <geodome@gmail.com>
2025-07-09 02:17:42 +0800

    added validation mechanism to AppConfig struct

--- Sources/AppConfig.swift
+++ Sources/AppConfig.swift
@@ -5,6 +5,8 @@ import Foundation
 enum AppConfigError: Error {
     case BadJson
     case NoConfigFile
+    case MissingAPIKey
+    case MissingDatabase
 }
 
 struct Config: Codable {
@@ -42,7 +44,15 @@ struct AppConfig {
             throw AppConfigError.NoConfigFile
         }
         if let jsonData = try? String(contentsOfFile: url.absoluteString).data(using: .utf8), let decoded = try? JSONDecoder().decode(Config.self, from: jsonData) {
-            config = decoded
+            guard decoded.APIKey.isEmpty else { 
+                logger.critical("Missing API Key")
+                throw AppConfigError.MissingAPIKey 
+            }
+            guard decoded.Database.isEmpty else { 
+                logger.critical("Missing Database File")
+                throw AppConfigError.MissingDatabase 
+            }
+            config = decoded 
         } else {
             logger.critical("unable to read or decode \(url.absoluteString)")
             throw AppConfigError.BadJson
