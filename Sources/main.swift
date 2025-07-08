d5072080054d2eb1158770cc2318b18a3c8ff28b
Donaldson Tan <geodome@gmail.com>
2025-07-09 01:55:57 +0800

    updated main.swift to use AppConfig init()

--- Sources/main.swift
+++ Sources/main.swift
@@ -6,16 +6,22 @@ import Foundation
 
 func main() {
     let logger = Logger(label:"main")
-    let configFile = URL(string:FileManager().currentDirectoryPath)!.appendingPathComponent("config.json")
-    if let config = getAppConfig(url:configFile), let db = config.getDatabaseConnection() {
-        // setup database
-        let tg = TgGroups(conn:db)
-        tg["ict133"] = ("Structured Programming", "https://t.me/+91udwbtlw1hmYjE9")
-        tg["ict162"] = ("Object Oriented Programming", "https://t.me/+91udwbtlw1hmYjE9")
-        print(config)
+    
+    let fm = FileManager()
+    let configFile = URL(string:fm.currentDirectoryPath)!.appendingPathComponent("config.json")
+    if fm.fileExists(atPath: configFile.absoluteString) {
+        if let config = try? AppConfig(url:configFile), let db = config.getDatabaseConnection() {
+            // setup database
+            let tg = TgGroups(conn:db)
+            tg["ict133"] = ("Structured Programming", "https://t.me/+91udwbtlw1hmYjE9")
+            tg["ict162"] = ("Object Oriented Programming", "https://t.me/+91udwbtlw1hmYjE9")
+            print(config)
+        } else {
+            logger.error("failed to connect to database. please check \(configFile.absoluteString)")
+            logger.info("Exiting app")
+        }
     } else {
-        logger.error("failed to connect to database. please check config.json")
-        logger.info("Exiting app")
+        logger.critical("cannot start. missing config file at \(configFile.absoluteString)")
     }
 }
 
