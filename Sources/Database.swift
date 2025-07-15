c98b8901a16cc920113663f05e370108daa21335
Donaldson Tan <geodome@gmail.com>
2025-07-15 11:01:59 +0800

    added createTables()

--- Sources/Database.swift
+++ Sources/Database.swift
@@ -3,13 +3,20 @@ import Foundation
 import SQLite
 
 class Database {
-    let logger = Logger(label: "Database")
-    let tTgGroups = Table("telegram_groups")
-    let uCode = Expression<String>("unit_code")
-    let uLink = Expression<String>("link")
+    private let logger = Logger(label: "Database")
+    private var db: Connection
+
     let tCourseInfo = Table("course_info")
+    let uCode = Expression<String>("unit_code")
     let uName = Expression<String>("unit_name")
-    var db: Connection
+    let link = Expression<String>("link")
+    let uDesc = Expression<String>("unit_desc")
+    let credit = Expression<Double>("credit")
+    let semester = Expression<String>("semester")
+    let special = Expression<Bool>("special")
+
+    let tPrereq = Table("course_prereq")
+    let uPrereq = Expression<String>("prereq")
 
     init(conn:Connection) {
         db = conn
@@ -20,21 +27,27 @@ class Database {
         let query1 = tCourseInfo.create { (t:TableBuilder) in 
             t.column(uCode, primaryKey: true)
             t.column(uName)
+            t.column(link)
+            t.column(uDesc)
+            t.column(credit)
+            t.column(semester)
+            t.column(special)
+
         }
         if let _ = try? db.execute(query1) {
             logger.info("course_info table created")
         } else {
             logger.info("course_info table exists")
-        }
+        } 
 
-        let query2 = tTgGroups.create { (t:TableBuilder) in 
+        let query2 = tPrereq.create { (t:TableBuilder) in 
             t.column(uCode, primaryKey: true)
-            t.column(uLink)
+            t.column(uPrereq, primaryKey: true)
         }
-        if  let _ = try? db.execute(query2) {
-            logger.info("telegram_groups table created")
+        if let _ = try? db.execute(query2) {
+            logger.info("course_prereq table created")
         } else {
-            logger.info("telegram_groups table exists")
-        }
+            logger.info("course_prereq table exists")
+        } 
     }
 }
