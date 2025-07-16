2dc7b804b6f64be880c6dca3d1870cbb3e79f365
Donaldson Tan <geodome@gmail.com>
2025-07-16 20:26:56 +0800

    added Database Layer

--- Sources/Database.swift
+++ Sources/Database.swift
@@ -1,26 +1,30 @@
 import Logging
-import Foundation
 import SQLite
 
-class Database {
+
+class DB {
     private let logger = Logger(label: "Database")
     private var db: Connection
 
     let tCourseInfo = Table("course_info")
     let uCode = Expression<String>("unit_code")
-    let uName = Expression<String>("unit_name")
-    let link = Expression<String>("link")
-    let uDesc = Expression<String>("unit_desc")
-    let credit = Expression<Double>("credit")
-    let semester = Expression<String>("semester")
-    let special = Expression<Bool>("special")
+    let uName = Expression<String?>("unit_name")
+    let link = Expression<String?>("link")
+    let uDesc = Expression<String?>("unit_desc")
+    let credit = Expression<Double?>("credit")
+    let semester = Expression<String?>("semester")
+    let special = Expression<Bool?>("special")
 
     let tPrereq = Table("course_prereq")
     let uPrereq = Expression<String>("prereq")
 
-    init(conn:Connection) {
-        db = conn
+    init(at path:String) {
+        db = try! Connection(path) 
         createTables()
+    }
+
+    func getConnection() -> Connection {
+        return db
     }
 
     private func createTables() {
@@ -32,7 +36,6 @@ class Database {
             t.column(credit)
             t.column(semester)
             t.column(special)
-
         }
         if let _ = try? db.execute(query1) {
             logger.info("course_info table created")
@@ -49,5 +52,9 @@ class Database {
         } else {
             logger.info("course_prereq table exists")
         } 
+    }
+
+    func getTelegramGroups() -> (Table, Expression<String>, Expression<String?>, Expression<String?>) {
+        return (tCourseInfo, uCode, uName, link)
     }
 }
