681ff3908d98a9ba187f1cba87e62db6dcd81959
Donaldson Tan <geodome@gmail.com>
2025-07-16 20:29:46 +0800

    added Database Later

--- Sources/main.swift
+++ Sources/main.swift
@@ -6,16 +6,18 @@ import Foundation
 
 func main() {
     let logger = Logger(label:"main")
-    let configFile = URL(string:"file://" + FileManager().currentDirectoryPath)!.appendingPathComponent("config.json")
-    if let config = try? AppConfig(from:configFile), let db = config.getDatabaseConnection() {
+    let configFile = URL(string: FileManager().currentDirectoryPath)!.appendingPathComponent("config.json")
+    if let config = try? AppConfig(from:configFile) {
         // setup database
-        let tg = TgGroups(conn:db)
-        // tg["ict133"] = ("Structured Programming", "https://t.me/+91udwbtlw1hmYjE9")
+        let db = config.getDatabase()
+        let tg = TgGroups(db)
+        tg["ict133"] = ("Structured Programming", "https://t.me/+91udwbtlw1hmYjE9")
         // tg["ict162"] = ("Object Oriented Programming", "https://t.me/+91udwbtlw1hmYjE9")
-        let (uname, link) = tg["ict133"]!
-        print("ict133: \(uname) at \(link)")
-        for code in tg.search(for:["struc"]) {
-            print("search programming \(code) \(tg[code]!.0)")
+        if let (uname, link) = tg["ict133"] {
+            print("ict133: \(uname) at \(link)")
+            for code in tg.search(for:["struc"]) {
+                print("search programming \(code) \(tg[code]!.0)")
+            }
         }
         print("There are \(tg.count) ICT units")
     } else {
